cc0b3f6d7ee570d64c6cc09d6c8aef49336f4a4a
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-02-09 12:27:32 -0500

    add Clothes Schema

--- models/clothes.js
+++ models/clothes.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
+const Clothes = new Schema(
+  {
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    style: { type: String, required: true },
+    category: { type: String, required: true },
+    size: { type: Array, required: true },
+    store: { type: Schema.Types.ObjectId, ref: 'Store' }
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Clothes', Clothes)
