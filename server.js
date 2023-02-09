f1b254a9751634e77191c32f74d967687d4f6ca6
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-02-09 12:11:49 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const routes = require('./routes')
+const db = require('./db')
+const logger = require('morgan')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(logger('dev'))
+
+app.use(express.json())
+
+app.use('/', routes)
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
