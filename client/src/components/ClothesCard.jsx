bc6cffdc45d2832a685f024024b5f7b597bfc9a7
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-02-14 14:25:53 -0500

    StoreCard component

--- client/src/components/ClothesCard.jsx
+++ client/src/components/ClothesCard.jsx
@@ -1,9 +1,9 @@
 const ClothesCard = (props) => {
 
   return (
-    <div className="card clothes-card">
+    <div className="card clothes-card" onClick={props.onClick}>
       <div className="img-wrapper">
-        <img src={props.image} alt="Clothes" />
+        <img src={props.image} alt="clothes-card" />
       </div>
       <div className="info-wrapper flex-row space">
         <h3>{props.name}</h3>
---
+++ client/src/components/StoreCard.jsx
@@ -0,0 +1,16 @@
+const StoreCard = (props) => {
+
+  return (
+    <div className="card" onClick={props.onClick}>
+      <div className="img-wrapper">
+        <img src={props.logo} alt="Store" />
+      </div>
+      <div className="info-wrapper flex-row">
+        <h3>{props.name}</h3>
+      </div>
+    </div>
+  )
+}
+
+
+export default StoreCard
